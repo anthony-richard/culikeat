b8a07ecf1c692580b8e2bc62dda937f2f695f69e
Anthony RICHARD <anthonyrichard55200@gmail.com>
2020-12-10 15:10:43 +0100

    features logout.php (controller + view) finish

--- Controllers/LogoutController.php
+++ Controllers/LogoutController.php
@@ -6,7 +6,10 @@ class LogoutController extends Controller{
         // quand l'utilisateur se déconnecte on doit détruire la session
         Session::destroy();
 
+        // on ajoute à data un tableau avec un titre 
+        // $data à suivre dans le Controller.php
         $data = array("title" => "Deconnexion");
+
         // nom de la view = logout
         // avec data un tableau qui a comme titre "Deconnexion"
         // render = permet le rendu de la view logout
--- Views/logoutView.php
+++ Views/logoutView.php
@@ -1,3 +1,4 @@
 <h1> Vous êtes en train de vous déconnectés... </h1>
+
 <!-- l'utilisateur est redirigé automatiquement mais s'il elle ne l'ai pas, elle peut cliquer sur le lien -->
 <h2><a href="index.php?action=home">Si vous n'êtes pas redirigez, cliquer ici !</a></h2>
