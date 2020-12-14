8a02c20889f543a52dcbf443285719bd270dbe74
Joshua <joshua.back55@gmail.com>
2020-12-14 13:33:46 +0100

    home remake

--- Views/homeView.php
+++ Views/homeView.php
@@ -1,1 +1,24 @@
-<h1>Accueil</h1>
+<script> function filterRestaurateur(motsRecherches) {
+    const section = document.getElementById("Restaurateur");
+
+    // On récupére tous les éléments possédant la classe '.Restaurateur'
+    const messages = sectionMessages.querySelectorAll(".Restaurateur"); //<--- querySelectorAll renvoit un tableau
+
+    // On liste chaque article de articles
+    messages.forEach(
+
+        // On s'intéresse à un article de articles
+        (article) => {
+
+
+            // On teste si contenuRecherche est dans contenuDescription
+            if (contenuDescription.indexOf(contenuRecherche) === -1) {
+                article.style.display = "none";
+            }
+            else {
+                article.style.display = "block";
+            }
+        });
+
+}
+</script>
