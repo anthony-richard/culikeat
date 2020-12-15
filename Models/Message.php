684fbdbcde4e0888e3187495d4e2c3ea10390264
Anthony RICHARD <anthonyrichard55200@gmail.com>
2020-12-15 13:08:06 +0100

    features Messages.php finish

---
+++ Models/Message.php
@@ -0,0 +1,92 @@
+<?php
+
+require_once("Mode.php");
+
+class Message extends BadMethodCallException
+{
+    // champs
+    private $id;
+    private $post;
+
+    public static $tableMessage = "messages";
+
+    public function __construct($data = array())
+    {
+        $this->hydrate($data);
+    }
+
+    /* ----------------------------------------------
+        Setters
+    -------------------------------------------------*/
+    public function setId($id)
+    {
+        $this->id = intval($id);
+    }
+
+    public function setPost($post)
+    {
+        $this->post = $post;
+    }
+
+    /* ----------------------------------------------
+        Getters
+    -------------------------------------------------*/
+    public function getId()
+    {
+        return intval($this->id);
+    }
+
+    public function getPost()
+    {
+        return $this->post;
+    }
+
+    /* ----------------------------------------------
+        méthodes
+    -------------------------------------------------*/
+    // On surcharge le parent
+    public function refresh()
+    {
+        parent::refreshModel(array("post" => $this->post));
+    }
+
+    //  methode qui renvoie un tableau de messages
+    public function getDataArray()
+    {
+        return array(
+            "post" => $this->getPost(),
+        );
+    }
+
+    // methode qui renvoie un message en fonction d'un id
+    public static function getMessageById()
+    {
+        // Appelle “findBy” de “Model”
+        $data = self::_findOneBy(self::$tableMessage, array("id" => $id));
+
+        if ($data) {
+            // On crée un message à partir des données
+            $message = new Message($data);
+            return $message;
+        } else {
+            echo "<p>/!\ Je n'ai pas pu récupérer le message </p>";
+            return null;
+        }  
+    }
+
+    // methode qui renvoie tous les messages
+    public static function getAllMessages()
+    {
+        // Appelle “findBy” de “Model”
+        $data = self::_findAllBy(self::$tableMessage, array());
+
+        if ($data) {
+            // On renvoie tous les messages trouvés
+            return $data;
+        } else {
+            echo "<p>/!\ Je n'ai pas pu récupérer les messages</p>";
+            return array();
+        }
+    
+    }
+}
