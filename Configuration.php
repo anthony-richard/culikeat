1fb3564fbe23bb8ce670a3e94bbc2e64085d6281
Anthony RICHARD <anthonyrichard55200@gmail.com>
2020-12-15 11:12:45 +0100

    new file configuration.php for connection and features in BDD.php finish

---
+++ Configuration.php
@@ -0,0 +1,10 @@
+<?php 
+
+class Configuration{
+
+    // pour se connecter à la BDD
+    public static $host = "localhost";
+    public static $nameBDD = "culikeat";
+    public static $username = "root";
+    public static $password = "";
+}
---
+++ Models/BDD.php
@@ -0,0 +1,73 @@
+<?php
+
+require_once("Configuration.php");
+
+class BDD
+{
+    // champs qui aura l'instance de notre classe
+    protected static $instance;
+
+    // instance de la PDO
+    protected static $bdd;
+
+    private static $host;
+    private static $nameBDD;
+    private static $username;
+    private static $password;
+
+    protected function __construct()
+    {
+        self::$host     = Configuration::$host;
+        self::$nameBDD  = Configuration::$nameBDD;
+        self::$username = Configuration::$username;
+        self::$password = Configuration::$password;
+        self::connect();
+    }
+
+    // methode qui permet de se connecter à la BDD
+    protected static function connect(){
+        $host       = self::$host;
+        $nameBDD    = self::$nameBDD;
+        $username   = self::$username;
+        $password   = self::$password;
+
+        try {
+            self::$bdd = new PDO("mysql:host={$host};dbname={$nameBDD};charset=utf8", $username, $password);
+        } catch (PDOException $e) {
+            echo 'Connexion échouée : ' . $e->getMessage();
+        }
+        return self::$bdd;
+    }
+
+    //  Renvoie une instance une fois créée, et la créer si elle n'existe pas
+    public static function getInstance(){
+        if(!isset($instance)){
+            self::$instance = new self;
+        }else{
+            return self::$instance;
+        }
+    }
+     
+    // pour executer une requete 
+    /**
+    * @param String $sql - Requête SQL
+    * @param Array $data - Tableau de paramètres, null de base
+    */
+    public static function executeRequest($sql, $data = null)
+    {
+        //  on doit prendre la BDD à laquelle on se connecte
+        $instance = self::getInstance();
+        $bdd = $instance::$bdd;
+
+        //  si data est vide ou non défini
+        if(isset($data) || sizeof($data)===0){
+            // On exécute la requête SQL sans avoir besoin de $data
+            $result = $bdd->query($sql);
+        } else {
+            // on fait la requete sql grace à data
+            $request = $bdd->prepare($sql);
+            $result = $request->execute($data);
+        }
+        return $result;
+    }
+}
